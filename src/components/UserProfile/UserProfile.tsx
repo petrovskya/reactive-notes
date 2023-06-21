9053e045c2d8bcef442314e7fc70f01300065bbd
petrovskya <petrovska.ya@icloud.com>
2023-06-21 10:29:33 +0300

    feat(ui): add user photo

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,29 +1,39 @@
 import { FC } from 'react';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
+import { UserPhoto } from 'assets';
 import { COMPONENT_TITLE } from 'config/types';
 
+import { StyledAvatar, StyledCard, StyledCardContent } from './styles';
 import { IUserProfileProps } from './types';
 
 const UserProfile: FC<IUserProfileProps> = ({
   user: { firstName, lastName, email, dateOfBirth },
 }) => (
-  <Card>
-    <CardContent>
-      <Typography gutterBottom variant='h2' component='p' color='secondary'>
-        {COMPONENT_TITLE.USER_PROFILE}
-      </Typography>
-      <Typography gutterBottom variant='h4' component='p' color='secondary'>
-        {firstName} {lastName}
-      </Typography>
-      <Typography gutterBottom variant='h4' component='p' color='secondary'>
-        {email}
-      </Typography>
-      <Typography gutterBottom variant='h6' component='span' color='secondary'>
-        {dateOfBirth}
-      </Typography>
-    </CardContent>
-  </Card>
+  <StyledCard>
+    <Typography gutterBottom variant='h2' component='p' color='secondary'>
+      {COMPONENT_TITLE.USER_PROFILE}
+    </Typography>
+    <StyledCardContent>
+      <StyledAvatar src={UserPhoto} />
+      <Box>
+        <Typography gutterBottom variant='h4' component='p' color='secondary'>
+          {firstName} {lastName}
+        </Typography>
+        <Typography gutterBottom variant='h4' component='p' color='secondary'>
+          {email}
+        </Typography>
+        <Typography
+          gutterBottom
+          variant='h6'
+          component='span'
+          color='secondary'
+        >
+          {dateOfBirth}
+        </Typography>
+      </Box>
+    </StyledCardContent>
+  </StyledCard>
 );
 
 export default UserProfile;
